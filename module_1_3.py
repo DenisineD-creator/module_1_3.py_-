728fda3df7a42f87881b72564513a2b3bc5fe0ad
DenisineD-creator <dburobin1@mail.ru>
2024-08-27 16:11:03 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+name = 'Name: Денис'
+print(name)
+
+age = 'Age: 48'
+print(age)
+age = 48
+a = 17
+New_age = 'New Age:'
+
+print(New_age,age + a)
+
+is_student = 'True'
+is_student ='Is Student: True'
+print(is_student)
